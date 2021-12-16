71766f28e251d994abc8be84bb7cf78ff5f5bb7e
xiaoma <xiao.ma>
2021-12-16 09:28:41 +0800

    first commit

---
+++ src/main/java/com/ma/CodeMyBatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.ma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMyBatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeMyBatisPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ma/CodeMyBatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMyBatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
